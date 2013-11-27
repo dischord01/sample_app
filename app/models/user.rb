699e645418f85be82d9a9523135b329523543013
dischord01 <brad.sollar@imc-informatics.com>
2013-11-27 14:33:03 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
+
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,9 +8,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
    has_secure_password
    validates :password, length: { minimum: 6 }
-=======
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
->>>>>>> modeling-users
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,21 @@ require 'spec_helper'
 describe User do
 
   before do
-<<<<<<< HEAD
+
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
-=======
-    @user = User.new(name: "Example User", email: "user@example.com")
->>>>>>> modeling-users
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-<<<<<<< HEAD
+
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
-=======
->>>>>>> modeling-users
+
 
   it { should be_valid }
 
@@ -29,26 +25,17 @@ describe User do
     before { @user.name = " " }
     it { should_not be_valid }
   end
-<<<<<<< HEAD
-=======
 
->>>>>>> modeling-users
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
-<<<<<<< HEAD
-=======
 
->>>>>>> modeling-users
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-<<<<<<< HEAD
-=======
 
->>>>>>> modeling-users
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
@@ -69,7 +56,7 @@ describe User do
       end
     end
   end
-<<<<<<< HEAD
+
  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -111,7 +98,3 @@ describe User do
     end
   end
 end
-=======
-
-end
->>>>>>> modeling-users
